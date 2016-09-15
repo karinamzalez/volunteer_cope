e704b49bfe5f0b641268b345d06ca86b34f58200
Karina Gonzalez <karinamzalez@gmail.com>
2016-09-15 13:47:33 -0600

    fixe creation of user in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
     where(uid: auth_info[:id]).first_or_create do |new_user|
       new_user.uid                = auth_info[:id]
       new_user.name               = auth_info[:name]
-      new_user.screen_name        = auth_info[:login]
-      new_user.image              = auth_info[:avatar_url]
       new_user.email              = auth_info[:email]
+      new_user.username           = auth_info[:login]
+      new_user.image              = auth_info[:avatar_url]
       new_user.github             = auth_info[:html_url]
       new_user.oauth_token        = oauth.access_token
     end
