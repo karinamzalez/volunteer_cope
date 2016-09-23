a6d3413fe01bfcbcc201064ab0f7e2a85c0dedbc
Karina Gonzalez <karinamzalez@gmail.com>
2016-09-23 08:47:33 -0600

    add testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,16 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'capybara/rails'
+require 'database_cleaner'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
+# Add additional
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
