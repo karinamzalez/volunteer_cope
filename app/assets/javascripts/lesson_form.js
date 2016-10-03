8f62a1eceb1b221737c6da87ff91ab7f2cf567ad
Karina Gonzalez <karinamzalez@gmail.com>
2016-10-03 11:48:42 -0600

    change lesson form back to a js file as oppposed to an es6 file

--- app/assets/javascripts/lesson_form.js
+++ app/assets/javascripts/lesson_form.js
@@ -2,6 +2,7 @@
 $(document).ready(function () {
   function toggleView (lessonID, formID){
     $(lessonID).on("click", function(e){
+      console.log("apple");
       var lesson = e.target;
       var date = lesson.getAttribute("data-date");
       $("#lesson_date").val(date);
@@ -32,7 +33,7 @@ $(document).ready(function () {
 
   var viewLesson = function(){
     $(".view-lesson").on("click", function(){
-      var id = $(this).data("id");
+      var id = this.getAttribute("data-id");
       $.ajax({
         method: "GET",
         url: "/api/v1/lessons/" + id,
@@ -57,7 +58,6 @@ $(document).ready(function () {
           success: function(lessons){
             updateCalendarDates(date);
             renderViewLessonButtons(lessons);
-            console.log(lessons);
           }
         });
       }
@@ -67,8 +67,26 @@ $(document).ready(function () {
   function renderViewLessonButtons(lessons) {
     lessons.forEach(function(lesson){
       if(lesson[1] !== null){
-        dayLesson = lesson[0] + 1;
-        $(`.day_${dayLesson}_5`).removeAttr("data-id");
+        console.log(lesson[1]);
+        if ($(`.day_${lesson[0]}_5`).length === 1) {
+          var viewLesson = $(`.day_${lesson[0]}_5`)[0];
+          viewLesson.removeAttribute("data-id");
+          viewLesson.setAttribute("data-id", lesson[1][lesson[1].length - 1].id);
+        }
+        else {
+          var viewLesson2 = $(`.day_${lesson[0]}_5`)[1];
+          viewLesson2.setAttribute("data-id", lesson[1][lesson[1].length - 1].id);
+          viewLesson2.style.display = "";
+          $(`.day_${lesson[0]}_5`)[0].remove();
+        }
+      }
+      else {
+        var lessonButton = $(`.day_${lesson[0]}_5`)[0];
+        lessonButton.removeAttribute("data-id");
+        lessonButton.style.display = "none";
+        var date = $(lessonButton).data("date");
+        $(lessonButton).before('<button type="button" class="day_' + lesson[0] + '_5 btn btn-lesson btn-sm lesson" data-date="'+ date +'">Add Lesson</button>');
+        toggleView(".lesson", "#form");
       }
     });
   }
