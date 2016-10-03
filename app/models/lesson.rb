47fd2f460ee5a9e5e505790e814b56bc24bb810f
Karina Gonzalez <karinamzalez@gmail.com>
2016-10-03 13:21:05 -0600

    change token params within github_oauth service * add production conditional

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,5 @@
 class Lesson < ApplicationRecord
   def self.create_lesson(title, body)
-    byebug
     `curl -H "Content-Type: application/json" -H "Authorization: token #{ENV['ADMIN_TOKEN']}" --data '{"title":"#{title}", "body":"#{body}"}' https://api.github.com/repos/volunteercope/volunteer_cope/issues`
   end
 
